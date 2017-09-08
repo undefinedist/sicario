b5b5ae7f1a0e1562ac233570243bac096c519b29
Eung Jin Lee <oiojin831@gmail.com>
2017-09-08 20:46:23 +0900

    Gallery

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Carousel} from 'react-responsive-carousel'
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
+
+function Gallery({images}) {
+  return (
+    <Carousel axis="horizontal" showArrows dynamicHeight>
+      {images.map(image => (
+        <div>
+          <img src={image} />
+        </div>
+      ))}
+    </Carousel>
+  )
+}
+
+Gallery.propTypes = {
+  images: PropTypes.array,
+}
+
+Gallery.defaultProps = {
+  images: [
+    'http://lorempixel.com/400/300/',
+    'http://lorempixel.com/400/300/',
+    'http://lorempixel.com/400/300/',
+  ],
+}
+
+export default Gallery
---
+++ src/docs/examples/Gallery/ExampleGallery.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Gallery from 'sicario/Gallery'
+
+/** Gallery */
+export default function ExampleGallery() {
+  return <Gallery />
+}
